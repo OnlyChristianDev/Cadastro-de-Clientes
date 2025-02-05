fa4094162635193cac92a9bfa3f8d3de7c26ab92
ChristianAtena <christian.silva@atenaagencia.com>
2025-02-05 14:39:22 -0300

    cadastro de pf html e css

--- src/components/PessoaJuridica.vue
+++ src/components/PessoaJuridica.vue
@@ -1,40 +1,43 @@
 <script setup>
 import { ref } from 'vue'
 import InputsCadastro from './InputsCadastro.vue';
-
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 </script>
 <template>
     <div class="flex flex-col items-center gap-3">
-        <div>
-            <InputsCadastro placeholder="Razao Social"/>
-            <InputsCadastro placeholder="Nome fantasma"/>
+        <div class="flex items-center gap-3 mr-4">
+            <InputsCadastro icon="business-time"  placeholder="Razao Social"/>
+            <InputsCadastro icon="business-time" placeholder="Nome fantasma"/>
         </div>
-        <div>
-            <InputsCadastro placeholder="CNPJ"/>
-            <InputsCadastro placeholder="Inscriçao estadual"/>
+        <div class="flex items-center gap-3 mr-4">
+            <InputsCadastro icon="business-time" placeholder="CNPJ"/>
+            <InputsCadastro icon="business-time" placeholder="Inscriçao estadual"/>
+        </div class="flex items-center gap-3">
+        <div class="flex items-center gap-3 mr-1">
+            <InputsCadastro icon="user" placeholder="nome do responsavel"/>
+            <InputsCadastro icon="user" placeholder="cargo do responsavel"/>
         </div>
-        <div>
-            <InputsCadastro placeholder="nome do responsavel"/>
-            <InputsCadastro placeholder="cargo do responsavel"/>
-        </div>
-       <div>
-            <InputsCadastro placeholder="telefone da empresa"/>
-            <InputsCadastro placeholder="email da empresa"/>
+       <div class="flex items-center gap-3 mr-2">
+            <InputsCadastro icon="phone" placeholder="telefone da empresa"/>
+            <InputsCadastro icon="envelope" placeholder="email da empresa"/>
        </div>
-       <div>
-        <InputsCadastro placeholder="CEP"/>
-        <InputsCadastro placeholder="Rua"/>
+       <div class="flex items-center gap-3">
+        <InputsCadastro icon="location-dot" placeholder="CEP"/>
+        <InputsCadastro icon="location-dot" placeholder="Rua"/>
        </div>
-       <div>
-            <InputsCadastro placeholder="Numero"/>
-            <InputsCadastro placeholder="Bairro"/>
+       <div class="flex items-center gap-3">
+            <InputsCadastro icon="location-dot" placeholder="Numero"/>
+            <InputsCadastro icon="location-dot" placeholder="Bairro"/>
        </div>
-       <div>
-            <InputsCadastro placeholder="Cidade"/>
-            <InputsCadastro placeholder="Estado"/>
+       <div class="flex items-center gap-3 mr-2">
+            <InputsCadastro icon="city" placeholder="Cidade"/>
+            <InputsCadastro icon="location-dot" placeholder="Estado"/>
        </div>
-        <textarea class="bg-gray-100" name="" id=""></textarea>
-        <button>Cadastrar</button>
+       <div class="flex gap-2">
+        <FontAwesomeIcon icon="file" class="mt-2 text-gray-300" />
+            <textarea class="bg-gray-200 mx-auto resize-none h-[130px] p-2 w-[534px] outline-none" name="" id=""></textarea>
+       </div>
+        <button class="cursor-pointer mt-2 bg-blue-800 text-white rounded-md h-[50px] w-[300px] p-1">Cadastrar</button>
     </div>
     
 </template>
--- src/plugins/font-awasome.ts
+++ src/plugins/font-awasome.ts
@@ -1,9 +1,9 @@
 // src/plugins/font-awesome.ts
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faUser, faEnvelope, faHome, faPhone, faLocationDot, faCalendar, faFile} from '@fortawesome/free-solid-svg-icons';
+import { faUser, faEnvelope, faHome, faPhone, faLocationDot, faCalendar, faFile, faCity, faBusinessTime} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-library.add(faUser, faEnvelope, faHome, faPhone, faLocationDot, faCalendar, faFile);
+library.add(faCity, faUser, faEnvelope, faHome, faPhone, faLocationDot, faCalendar, faFile, faBusinessTime);
 
 export default (app: any) => {
   app.component('font-awesome-icon', FontAwesomeIcon);
